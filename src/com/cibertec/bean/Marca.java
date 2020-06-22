e5c275271bd6c4faf607c5636798531c83dc2808
Jose <Jose@DESKTOP-SB2MCTO.lan>
2020-06-22 18:22:58 -0500

    se Agrego comit

---
+++ src/com/cibertec/bean/Marca.java
@@ -0,0 +1,8 @@
+package com.cibertec.bean;
+
+public class Marca {
+	 private int  idmarca;
+	 private String nombre,estado;
+	 private Pais idpais;
+
+}
---
+++ src/com/cibertec/bean/Pais.java
@@ -0,0 +1,15 @@
+package com.cibertec.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Pais {
+	
+	private int idpais;
+	private String iso,nombre;
+	
+	
+	
+
+}
